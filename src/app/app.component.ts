dea5b02f0a39cb842529934ff03e55acbbd97013
Akjol Jaenbai, David Zhu and Julius Bade <akjol@>
2018-10-23 11:13:58 -0700

    finsh the first part,add the Recipe constructor,tittle...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe';
+  recipies :Recipe []=[
+    new Recipe('Spicy Grilled Shrimp',['1 large clove garlic','1 tablespoon coarse salt','1/2 teaspoon cayenne pepper', '1 teaspoon paprika'],'Cook that '),
+    new Recipe('Grilled Chicken',['chicken', 'salt', 'pepper'],'Grill that '),
+    new Recipe('Salt Onions',['onions', 'salt'],'Salt that '),
+  ]
+  // showIngredient() {
+  //  let flag=true;
+  // }
+ 
+    show: boolean = true;
+  
 }
+
